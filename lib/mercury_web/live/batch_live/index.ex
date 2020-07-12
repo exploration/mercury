fb04bed51a98d143536a5765b4be3f1e6bfb3e7f
Donald Merand <dmerand@explo.org>
2020-07-12 17:31:06 -0400

    make row view friendlier

--- lib/mercury_web/live/batch_live/index.ex
+++ lib/mercury_web/live/batch_live/index.ex
@@ -43,17 +43,22 @@ defmodule MercuryWeb.BatchLive.Index do
     end
   end
 
-  def handle_event("recover", %{"batch" => %{"table_data" => table_data}}, socket) do
-    {:noreply, update_table_data(socket, table_data)}
+  def handle_event("override_phase", %{"phase" => phase}, socket) do
+    state = %{socket.assigns.state | phase: phase}
+    {:noreply, assign(socket, state: state)}
   end
-
+  
   def handle_event("parse_data", %{"batch" => %{"table_data" => table_data}}, socket) do
     {:noreply, update_table_data(socket, table_data)}
   end
 
-  def handle_event("override_phase", %{"phase" => phase}, socket) do
-    state = %{socket.assigns.state | phase: phase}
-    {:noreply, assign(socket, state: state)}
+  def handle_event("recover", %{"batch" => %{"table_data" => table_data}}, socket) do
+    {:noreply, update_table_data(socket, table_data)}
+  end
+
+  def handle_event("select_row", %{"row" => row}, socket) do
+    state = socket.assigns.state
+    {:noreply, assign(socket, :state, %{state | selected_row: String.to_integer(row)})}
   end
 
   def handle_event("validate_batch", %{"batch" => params}, socket) do
