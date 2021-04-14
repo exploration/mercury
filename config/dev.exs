d07264e05a331cdb894cc545710b6435b4d9d4e5
Donald Merand <dmerand@explo.org>
2021-04-14 14:00:16 -0400

    node surgery

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,6 @@ config :mercury, MercuryWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
